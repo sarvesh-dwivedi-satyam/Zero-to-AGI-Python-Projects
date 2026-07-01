fb07002f98032f16f632a247362ef52217428b96
Sarvesh Dwivedi <2000locomail@gmail.com>
2026-07-01 21:41:43 +0530

    Add extract_coordinates function to display coordinates from a tuple

---
+++ Tuple_Coordinates/Tuple_Coordinates.py
@@ -0,0 +1,9 @@
+def extract_coordinates():
+    location = (10,20)
+    x, y = location
+    print(f"Extracted X-Coordinate: {x}")
+    print(f"Extracted Y-Coordinate: {y}")
+    print(f"Full location: {location}")
+
+if __name__ == '__main__':
+    extract_coordinates()
