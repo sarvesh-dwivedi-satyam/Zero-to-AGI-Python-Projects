a0f80ccc7999875fdbd2946c0ad7e8d68c1eb3bc
Sarvesh Dwivedi <2000locomail@gmail.com>
2026-07-04 11:06:09 +0530

    Add scale_list_elements function to multiply list elements by 2

---
+++ List_Multiplication_Loop/List_Multiplication_Loop.py
@@ -0,0 +1,8 @@
+
+def scale_list_elements():
+    numbers = [5, 12, 18, 25, 30]
+    for num in numbers:
+        print(f"Orignal: {num} -> Multiplied: {num * 2}")
+
+if __name__ == '__main__':
+    scale_list_elements()
